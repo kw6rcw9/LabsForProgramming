15f28df7a6f6e7c153ede1fd21fef0b64ee13652
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-09-06 21:01:30 +0300

    final try to upload it on Git

---
+++ DynamicArray/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DynamicArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+        }
+    }
+}
