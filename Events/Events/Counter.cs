dd87d04713a76de1cde104445612c661869c7346
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-10-22 23:49:55 +0300

    finished task

--- Events/Events/Counter.cs
+++ Events/Events/Counter.cs
@@ -4,11 +4,16 @@ public class Counter
 {
     public delegate void MyDel();
     public event MyDel? OnCounterChange;
+    public event Func<int> OverFlow;
     public int CurrentCounter { get; private set; }
+    private const int _limit = 100;
 
     public void IncreaseCounter(int newNum)
     {
         CurrentCounter += newNum;
+        if(CurrentCounter >= _limit)
+             CurrentCounter = OverFlow.Invoke();
+        
         OnCounterChange?.Invoke();
     }
     
@@ -16,7 +21,7 @@ public class Counter
     {
         
         CurrentCounter -= newNum;
-        if (CurrentCounter < 0)
+        if (CurrentCounter <= 0)
             throw new Exception("Counter cannot be below or equal zero");
         OnCounterChange?.Invoke();
     }
--- Events/Events/Program.cs
+++ Events/Events/Program.cs
@@ -2,26 +2,56 @@
 
 Counter counter = new Counter();
 counter.OnCounterChange += NotifyAboutEvent;
-counter.OnCounterChange += CheckOnSimmpleNum;
+counter.OnCounterChange += CheckOnSimpleNum;
+counter.OverFlow += () =>
+{
+    Console.WriteLine("Counter was reseted");
+    return default;
+};
+//counter.IncreaseCounter(110);
 counter.IncreaseCounter(8);
 counter.DecreaseCounter(1);
 counter.IncreaseCounter(2);
 counter.DecreaseCounter(1);
+var answer = () =>
+{
+    Console.WriteLine("To unsubscribe enter yes");
+    return Console.ReadLine();
+};
+var action = (string s) =>
+{
+    if (s.ToLower() == "yes")
+    {
+        counter.OnCounterChange -= NotifyAboutEvent;
+        counter.OnCounterChange -= CheckOnSimpleNum;
+        Console.WriteLine("Unsubscribed");
+    }
+        
+};
+
+string a = answer()!;
+action(a);
 
 
 
 void NotifyAboutEvent() => Console.WriteLine("Event exists");
 
-void CheckOnSimmpleNum()
+void CheckOnSimpleNum()
 { 
     Console.WriteLine(counter.CurrentCounter);
- for (int i = 1; i <= counter.CurrentCounter; i++)
- {
-     if (counter.CurrentCounter % i == 0 && i != 1 && i != counter.CurrentCounter)
+    if(counter.CurrentCounter == 0)
+        return;
+    for (int i = 1; i <= counter.CurrentCounter; i++)
+    {
+     if (counter.CurrentCounter % i == 0 
+         && i != 1 
+         && i != counter.CurrentCounter)
      {
          return;
      }
 
- }
- Console.WriteLine("New counter is  simple");
+    }
+    Console.WriteLine("New counter is  simple");
 }
+
+
