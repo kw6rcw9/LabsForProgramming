2f849c67e40e356eaf2215c460d32f61052e4de3
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-10-28 19:11:10 +0300

    finished kt

--- SecondKT/SecondKT/ObservableIntStack.cs
+++ SecondKT/SecondKT/ObservableIntStack.cs
@@ -2,5 +2,49 @@
 
 public class ObservableIntStack
 {
-    
+    public event Action<int> OnItemAdded;
+    public event Action<int> OnItemRemoved;
+    private Stack<int> _stack;
+    public int Count => _stack.Count;
+
+    public ObservableIntStack(IEnumerable<int> nums)
+    {
+        _stack = new Stack<int>();
+        foreach (var i in nums)
+        {
+            _stack.Push(i);
+        }
+    }
+
+    public void Subscribe(Action<int> subscriber)
+    {
+        if (subscriber == null)
+            throw new ArgumentNullException();
+        OnItemAdded += subscriber;
+        OnItemRemoved += subscriber;
+    }
+    public void Unsubscribe(Action<int> subscriber)
+    {
+        if (subscriber == null)
+            throw new ArgumentNullException();
+        OnItemAdded -= subscriber;
+        OnItemRemoved -= subscriber;
+    }
+
+    public void Push(int item)
+    {
+        _stack.Push(item);
+        OnItemAdded?.Invoke(item);
+    }
+
+    public void Pop()
+    {
+        if (_stack == null)
+            throw new ArgumentException("Stack is empty");
+        OnItemRemoved?.Invoke(_stack.Pop());
+    }
+
+    public int Peek() => _stack.Peek();
+
+
 }
--- SecondKT/SecondKT/Program.cs
+++ SecondKT/SecondKT/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using SecondKT;
+
+ObservableIntStack stack = new ObservableIntStack(new []{1,2});
+
+stack.Subscribe(FirstSub);
+stack.Subscribe(SecondSub);
+
+
+stack.Push(3);
+stack.Push(4);
+stack.Push(5);
+stack.Pop();
+stack.Pop();
+stack.Unsubscribe(FirstSub);
+stack.Unsubscribe(SecondSub);
+
+
+void FirstSub(int num)
+{
+    Console.WriteLine("i`m the first sub " + num);
+}
+
+void SecondSub(int num)
+{
+    Console.WriteLine("i`m the second sub " + num);
+}
