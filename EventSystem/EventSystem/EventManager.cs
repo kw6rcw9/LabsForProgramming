bcc029229e5e4497b6f1498cfb27303068ade996
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-11-01 23:07:30 +0300

    started task

---
+++ EventSystem/EventSystem/EventManager.cs
@@ -0,0 +1,25 @@
+﻿namespace EventSystem;
+
+public static class EventManager
+{
+    private static Dictionary<string, Action> _eventsList;
+   
+    
+
+    public static void CreateEvent(string eventName)
+    {
+    }
+    public static void RemoveEvent(string eventName){}
+    public static void RaiseEvent(string eventName){}
+    public static void Clear(){}
+
+// Методы подписки и отписки
+
+// Здесь на месте Action может быть также и Action<T>,
+// где T - параметр любого типа 
+    public static void Subscribe(string eventName, Action callBack){}
+    public static void UnSubscribe(string eventName, Action callback){}
+
+
+    
+}
---
+++ EventSystem/EventSystem/Program.cs
@@ -0,0 +1,5 @@
+﻿
+
+using EventSystem;
+
+EventManager.CreateEvent("1");
