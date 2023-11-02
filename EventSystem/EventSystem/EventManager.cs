2cb9280f9f88704b3e1578f276010b39ff715c7d
kw6rcw9 <120956700+kw6rcw9@users.noreply.github.com>
2023-11-02 20:07:03 +0300

    finished task

--- EventSystem/EventSystem/EventManager.cs
+++ EventSystem/EventSystem/EventManager.cs
@@ -2,24 +2,72 @@
 
 public static class EventManager
 {
-    private static Dictionary<string, Action> _eventsList;
+    private static Dictionary<string, Action?> _eventsList = new Dictionary<string, Action?>();
    
-    
+
+
 
     public static void CreateEvent(string eventName)
     {
+        if (_eventsList.ContainsKey(eventName))
+            Exception("Событие с таким именем уже существует");
+            
+        _eventsList.Add(eventName, null);
+        
+
     }
-    public static void RemoveEvent(string eventName){}
-    public static void RaiseEvent(string eventName){}
-    public static void Clear(){}
 
-// Методы подписки и отписки
+    public static void RemoveEvent(string eventName)
+    {
+        if (!_eventsList.ContainsKey(eventName))
+            Exception("События с таким именем не существует");
+        
+        _eventsList.Remove(eventName);
+    }
 
-// Здесь на месте Action может быть также и Action<T>,
-// где T - параметр любого типа 
-    public static void Subscribe(string eventName, Action callBack){}
-    public static void UnSubscribe(string eventName, Action callback){}
+    public static void RaiseEvent(string eventName)
+    {
+        if (!_eventsList.ContainsKey(eventName))
+            Exception("События с таким именем не существует");
+        if(_eventsList[eventName] == null)
+            Exception("У события нет подписчиков");
+        
+        _eventsList[eventName]?.Invoke();
+        
+    }
 
+    public static void Clear()
+    {
+        _eventsList.Clear();
+    }
+    
+    public static void Subscribe(string eventName, Action callBack)
+    {
+        if (!_eventsList.ContainsKey(eventName))
+            Exception("События с таким именем не существует");
+        if (_eventsList[eventName] == null)
+            _eventsList[eventName] = callBack;
+        else
+        {
+            _eventsList[eventName] += callBack;
+        }
+            
 
+    }
+    
+
+    public static void UnSubscribe(string eventName, Action callback)
+    {
+        if (!_eventsList.ContainsKey(eventName))
+            Exception("События с таким именем не существует");
+        _eventsList[eventName] -= callback;
+
+    }
+
+    private static void Exception(string message) =>
+        throw new ArgumentException(message);
+    
+        
+    
     
 }
---
+++ EventSystem/EventSystem/MyClass.cs
@@ -0,0 +1,12 @@
+﻿namespace EventSystem;
+
+public class MyClass
+{
+    public MyClass()
+    {
+        EventManager.Subscribe("AttackWaveStart", delegate {  });
+        EventManager.Subscribe("AttackWaveEnd", delegate {  });
+        EventManager.Subscribe("DateTime", () => Console.WriteLine(DateTime.Now));
+        
+    }
+}
